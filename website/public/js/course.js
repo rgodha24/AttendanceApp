7dcf6a25433a9478109c37cfa625453f8fa66e04
Jack Venberg <jvenberg@gmail.com>
2017-05-08 20:57:53 -0700

    Finished prototype of web interface

--- website/public/js/course.js
+++ website/public/js/course.js
@@ -1,43 +1,171 @@
 var course_number = document.getElementById('course_number')
 var link_url = document.getElementById('link_url')
 
- var database = firebase.database();
+var database = firebase.database();
 
 course_number.oninput = function() {
-   link_url.href = "https://brophyprep.instructure.com/api/v1/courses/"+course_number.value+"/users?per_page=100"
+    link_url.href = "https://brophyprep.instructure.com/api/v1/courses/"+course_number.value+"/users?per_page=100"
 };
 
+var course_to_delete = "";
+var student_to_delete = "";
+var student_course_to_delete = "";
 
 var fr = new FileReader();
 
 function uploadFile() {
-	var selectedFile = document.getElementById('uploaded_file').files[0];
-	fr.readAsText(selectedFile)
+    var selectedFile = document.getElementById('uploaded_file').files[0];
+    fr.readAsText(selectedFile)
 }
 
 function add_course(name, period_num, course_data) {
-  firebase.database().ref('courses/'+name).set({
-    "name": name,
-    "period_num": period_num,
-    "students": course_data
+    var id = name.replace(/([^a-z0-9]+)/gi, '-');
+    checkIfUserExists(id, name, period_num, course_data);
+}
+
+
+function userExistsCallback(id, name, period_num, course_data, exists) {
+    if (exists) {
+        Materialize.toast("Class Failed To Load (Already Exists)", 4000);
+    } else {
+        firebase.database().ref('courses/'+id).set({
+            "id": id,
+            "name": name,
+            "period_num": period_num,
+            "students": course_data
+        });
+        Materialize.toast("Class Loaded Successfully", 4000);
+    }
+}
+
+function checkIfUserExists(id, name, period_num, course_data) {
+    firebase.database().ref("courses/"+id).once('value', function(snapshot) {
+    var exists = (snapshot.val() !== null);
+    userExistsCallback(id, name, period_num, course_data, exists);
   });
+}
+
+function update_courses(data) {
+    var course_list = document.getElementById("course_list");
+    course_list.innerHTML = '';
+    for (var key in data){
+        var course = data[key];
+
+        var list_element = document.createElement("li");
+
+        var header = document.createElement("div");
+        header.className = "collapsible-header";
+        header.setAttribute('course_id', course["id"]);
+        var name_header = document.createElement("div");
+        name_header.innerHTML = course["name"] + " - Period " + course["period_num"];
+
+        var delete_header = document.createElement("div");
+        delete_header.innerHTML = "<i onclick='delete_course_dialog(this)' class='material-icons'>clear</i>";
+        delete_header.style.float = "right";
+
+        header.appendChild(delete_header);
+        header.appendChild(name_header);
+
+
+        var students = course["students"]
+        var table = document.createElement("table");
+        table.className = "striped"
+        var thead = document.createElement("thead");
+        var tr = document.createElement("tr");
+        
+        var sort_name = document.createElement("th");
+        sort_name.innerHTML = "Sortable Name";
+        var name = document.createElement("th");
+        name.innerHTML = "Name";
+        var student_id = document.createElement("th");
+        student_id.innerHTML = "Student ID";
+        var delete_heading = document.createElement("th");
+        delete_heading.innerHTML = "";
+        
+        tr.appendChild(sort_name);
+        tr.appendChild(name);
+        tr.appendChild(student_id);
+
+        thead.appendChild(tr);
+        table.appendChild(thead);
+
+        var tbody = document.createElement("tbody");
+        for (var key in students) {
+            var student = students[key];
+            var tr = document.createElement("tr");
+
+            var td = document.createElement("td");
+            td.innerHTML = student["sortable_name"]
+            tr.appendChild(td);
+
+            var td = document.createElement("td");
+            td.innerHTML = student["name"]
+            tr.appendChild(td);
+
+            var td = document.createElement("td");
+            td.innerHTML = student["sis_user_id"]
+            tr.appendChild(td);
+
+            var delete_student = document.createElement("td");
+            delete_student.innerHTML = "<i style='cursor: pointer' onclick='delete_student_dialog(this)' class='material-icons'>clear</i>";
+            tr.appendChild(delete_student);
+
+            tbody.appendChild(tr);
+        }
+
+        table.appendChild(tbody);
+
+        var body = document.createElement("div");
+        body.className = "collapsible-body";
+        body.appendChild(table);
+
+        list_element.appendChild(header);
+        list_element.appendChild(body);
+        course_list.appendChild(list_element);
+    }
+
+     $('.collapsible').collapsible();
+}
+
+function delete_course_dialog(element) {
+    element.parentElement.click();
+    $('#delete_course_modal').modal('open');
+    course_to_delete = element.parentElement.parentElement.getAttribute("course_id")
+}
+
+function delete_course() {
+    console.log('courses/'+course_to_delete)
+    firebase.database().ref('courses/'+course_to_delete).remove();
+    Materialize.toast("Course Successfully Deleted", 4000);
+}
+
+function delete_student_dialog(element) {
+    $('#delete_student_modal').modal('open');
+    student_course_to_delete = element.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.firstChild.getAttribute("course_id");
+    student_to_delete = element.parentElement.parentElement.childNodes[2].innerHTML
+}
+
+function delete_student() {
+    console.log('courses/'+student_course_to_delete)
+    firebase.database().ref('courses/'+student_course_to_delete+"/students/"+student_to_delete).remove();
+    Materialize.toast("Student Successfully Deleted", 4000);
 }
 
 fr.onload = function(e) {
     var course_data = JSON.parse(e.target.result.substring(9))
     if (course_data[0].sis_user_id == null) {
-	 	Materialize.toast('Invalid course data', 4000)
-	 	return
+         Materialize.toast('Invalid course data', 4000)
+         return
     }
     var filtered_course_data = {}
     for (var i = 0; i < course_data.length; i++) {
-    	delete course_data[i].id
-    	delete course_data[i].integration_id
-    	delete course_data[i].login_id
-    	delete course_data[i].root_account
-    	delete course_data[i].short_name
-    	delete course_data[i].sis_login_id
-    	filtered_course_data[course_data[i].sortable_name] = course_data[i];
+        delete course_data[i].id
+        delete course_data[i].integration_id
+        delete course_data[i].login_id
+        delete course_data[i].root_account
+        delete course_data[i].short_name
+        delete course_data[i].sis_login_id
+        filtered_course_data[course_data[i].sis_user_id] = course_data[i];
     }
     var course_name = document.getElementById("course_name").value
     var course_period = document.getElementById("period_number").value
@@ -45,17 +173,17 @@ fr.onload = function(e) {
     // console.log(course_name,course_period)
     add_course(course_name, course_period, filtered_course_data)
 
-	console.log(filtered_course_data)
+    // console.log(filtered_course_data)
 };
 
 var courseRef = firebase.database().ref('courses');
-courseRef.on('value', function(snapshot) {
-  // updateStarCount(postElement, snapshot.val());
-  console.log("Test")
+    courseRef.on('value', function(snapshot) {
+    update_courses(snapshot.val());
 });
 
 
 
 $(document).ready(function() {
-	$('select').material_select();
+    $('select').material_select();
+    $('.modal').modal();
 });
--- website/public/js/main.js
+++ website/public/js/main.js
@@ -1,9 +1,13 @@
 var dateSlider = document.getElementById("sliderTime");
 
+var database = firebase.database();
+
+var current_course = "";
+var current_time = [];
+
 function timestamp(str){
     return new Date(str).getTime();   
 }
-
 
 console.log(new Date(0).getTime())
 
@@ -11,7 +15,7 @@ noUiSlider.create(dateSlider, {
 // Create two timestamps to define a range.
   range: {
         min: new Date(0, 0, 0, 7, 0, 0).getTime(),
-        max: new Date(0, 0, 0, 15, 0, 0).getTime()
+        max: new Date(0, 0, 0, 23, 0, 0).getTime()
     },
 
   connect: true,
@@ -47,12 +51,23 @@ function formatDate(date) {
   return h + ':' + m + " " + end;
 }
 
+function raw_format_date(date) {
+	h = date.getHours();
+	m = (date.getMinutes()<10?'0':'') + date.getMinutes();
+	return h + ':' + m;
+}
+
 dateSlider.noUiSlider.on('update', function( values, handle ) {
   dateValues[handle].innerHTML = formatDate(new Date(+values[handle]));
+  document.getElementById('hour7').value = '["'+raw_format_date(new Date(+values[0]))+'", "'+raw_format_date(new Date(+values[1]))+'"]';
 });
 
 dateSlider.noUiSlider.on('start', function( values, handle ){
   document.getElementById("hour7").checked = true;
+});
+
+dateSlider.noUiSlider.on('end', function( values, handle ){
+  force_update();
 });
 
 times = [["8:00","8:55", "hour1"],
@@ -62,19 +77,154 @@ times = [["8:00","8:55", "hour1"],
          ["12:05","13:50", "hour5"],
          ["13:50","14:45", "hour6"]]
 
-console.log(times[0][0])
+var courseRef = firebase.database().ref();
+    courseRef.on('value', function(snapshot) {
+    update_courses(snapshot.val());
+    update_lists(snapshot.val());
+});
 
-for (var i = 0; i < times.length; i++) {
-  if (checkBetween(times[i])) {
-    document.getElementById(times[i][2]).checked = true;
-    break;
-  }
-  document.getElementById("hour7").checked = true;
+
+function update_courses(data) {
+    var course_list = document.getElementById("course_select");
+    course_list.innerHTML = '';
+    data = data["courses"];
+    for (var key in data){
+        var course = data[key];
+
+        var option_element = document.createElement("option");
+        option_element.value = course["id"]
+        option_element.innerHTML = course["name"] + " - Period " + course["period_num"];
+
+        course_list.appendChild(option_element);
+    }
+	$('select').material_select();
 }
 
-function checkBetween(timeArray) {
+function force_update() {
 
-  console.log(timeArray)
+	firebase.database().ref().once('value', function(snapshot) {
+	    update_lists(snapshot.val())
+	});
+}
+
+$('#courses_selector_container').on('change', 'select', function(){ force_update(); });
+
+function update_lists(data) {
+	var rates = document.getElementsByName('time');
+	var rate_value;
+	for(var i = 0; i < rates.length; i++){
+	    if(rates[i].checked){
+	        rate_value = rates[i].value;
+	    }
+	}
+
+
+	current_time = JSON.parse(rate_value);
+	current_course = document.getElementById("course_select").value;
+
+	var tbody_absent = document.getElementById("absent_table");
+	var tbody_present = document.getElementById("present_table");
+	tbody_absent.innerHTML = '';
+	tbody_present.innerHTML = '';
+
+	var date = new Date();
+	var month = ((date.getMonth()+1)<10?'0':'') + (date.getMonth()+1);
+	var day = (date.getDate()<10?'0':'') + date.getDate();
+
+	sign_in_today = data["sign-in"][date.getFullYear()][month][day]
+
+	// console.log(check_if_signed_in(17240, ["21:00","23:00"], sign_in_today))
+
+	course = data["courses"][current_course]["students"];
+	for (var key in course){
+        var student = course[key];
+        var tr = document.createElement("tr");
+
+		var td = document.createElement("td");
+        td.innerHTML = student["sortable_name"]
+        tr.appendChild(td);
+
+        if (check_if_signed_in(student["sis_user_id"], current_time, sign_in_today)) {
+			var td = document.createElement("td");
+	        td.innerHTML = student["sis_user_id"]
+	        tr.appendChild(td);
+
+	        var td = document.createElement("td");
+	        td.innerHTML = student["sis_user_id"]
+	        tr.appendChild(td);
+
+	        tbody_present.appendChild(tr);
+
+        } else {
+        	var td = document.createElement("td");
+	        td.innerHTML = student["name"]
+	        tr.appendChild(td);
+
+	        var td = document.createElement("td");
+	        td.innerHTML = student["sis_user_id"]
+	        tr.appendChild(td);
+
+	        tbody_absent.appendChild(tr);
+        }
+    }
+	
+}
+
+function check_if_signed_in(student_id, current_time, sign_in_today) {
+	sign_ins_times = []
+	startTime = current_time[0]
+  	endTime = current_time[1]
+	
+	for (var key in sign_in_today){
+		var sign_in_event = sign_in_today[key];
+		if (sign_in_event.id == student_id) {
+			sign_ins_times.push(sign_in_event.time)
+		}
+	}
+
+
+  	startDate = new Date();
+  	startDate.setHours(startTime.split(":")[0]);
+  	startDate.setMinutes(startTime.split(":")[1]);
+	  	
+  	endDate = new Date();
+  	endDate.setHours(endTime.split(":")[0]);
+  	endDate.setMinutes(endTime.split(":")[1]);
+
+
+	for (var i = 0; i < sign_ins_times.length; i++) {
+		sign_ins_times[i];
+		if (startDate < currentDate && endDate >= currentDate) {
+			return true;
+		}
+	}
+	return false;
+}
+
+$(document).ready(function() {
+  for (var i = 0; i < times.length; i++) {
+    if (checkBetween(times[i])) {
+      document.getElementById(times[i][2]).checked = true;
+      break;
+    }
+  }
+  document.getElementById("hour7").checked = true;
+
+
+  	var rad = document.getElementsByName("time");
+    var prev = null;
+    for(var i = 0; i < rad.length; i++) {
+        rad[i].onclick = function() {
+            // (prev)? console.log(prev.value):null;
+            if(this !== prev) {
+                prev = this;
+            }
+            force_update()
+        };
+    }
+});
+
+function checkBetween(timeArray) {
   startTime = timeArray[0]
   endTime = timeArray[1]
   currentDate = new Date()
