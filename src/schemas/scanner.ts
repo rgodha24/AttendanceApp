03062137996631cc28cfd2f7a348a42ac5446e4b
Rohan Godha <rgodha24@brophybroncos.org>
2022-11-18 15:17:00 -0700

    better addScanner schema

--- src/schemas/scanner.ts
+++ src/schemas/scanner.ts
@@ -6,8 +6,20 @@ const scannerSchema = z.object({
       return await trpc
          .query("misc.check-scanner-name", { name: scannerName })
          .then((res) => res !== "available");
-   }),
-   scannerSecret: z.string(),
+   }).refine(
+      (data) => {
+         // return false if data.name has any spaces in it
+         return !data.includes(" ");
+      },
+      { message: "class name shouldn't have spaces" }
+   ),
+   scannerSecret: z.string().refine(
+      (data) => {
+         // return false if data.name has any spaces in it
+         return !data.includes(" ");
+      },
+      { message: "class name shouldn't have spaces" }
+   ),
 });
 
 export default scannerSchema;
